05a16cd692240a0bbfca4399bf71890f8928e129
Luke Towell <luke.towell@hotmail.com>
2020-08-15 13:53:30 +0100

    First pass at the storeItem
    - first pass at the store item model
    - TODO: add the primary key relationships

---
+++ src/main/java/com/luketowell/webservices/models/StoreItem.java
@@ -0,0 +1,32 @@
+package com.luketowell.webservices.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "store_item")
+public class StoreItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="item_upc")
+    private String item_upc;
+
+    @Column(store_id)
+    private String store_id;
+
+    public StoreItem() {
+    }
+
+    public StoreItem(Long id, String item_upc, String store_id) {
+        this.id = id;
+        this.item_upc = item_upc;
+        this.store_id = store_id;
+    }
+}
