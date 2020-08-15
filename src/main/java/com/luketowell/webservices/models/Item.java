beaf2b3a13536071e2f94f8e567dd33473de44f0
Luke Towell <luke.towell@hotmail.com>
2020-08-15 13:12:48 +0100

    Updated the item model to correspond with the table
    - added all table based annotations

--- src/main/java/com/luketowell/webservices/models/Item.java
+++ src/main/java/com/luketowell/webservices/models/Item.java
@@ -7,26 +7,41 @@ import java.sql.Date;
 
 @Data
 @Entity
+@Table(name="item")
 public class Item {
 
     @Id
+    @Column(name="upc")
     private String upc;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="description")
     private String description;
+
+    @Column(name="expiry")
     private Date expiry;
-    private Integer category_id;
+
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable=false)
+    private ItemCategory category ;
+
+    @Column(name="size")
     private String size;
+
+    @Column(name="img_url")
     private String img_url;
 
     public Item() {
     }
 
-    public Item(String upc, String name, String description, Date expiry, Integer category_id, String size, String img_url) {
+    public Item(String upc, String name, String description, Date expiry, ItemCategory category, String size, String img_url) {
         this.upc = upc;
         this.name = name;
         this.description = description;
         this.expiry = expiry;
-        this.category_id = category_id;
+        this.category = category;
         this.size = size;
         this.img_url = img_url;
     }
