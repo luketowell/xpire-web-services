f50a4a4e174034094f99f17d970c89421aa16d20
Luke Towell <luke.towell@hotmail.com>
2020-08-17 14:40:09 +0100

    updated the store.java
    - updated store values to return attributes under different names.

--- src/main/java/com/luketowell/webservices/models/Store.java
+++ src/main/java/com/luketowell/webservices/models/Store.java
@@ -1,47 +1,29 @@
 package com.luketowell.webservices.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table(name="store")
 public class Store {
 
     @Id
-    private String id;
+    @Column(name="id")
+    private String value;
 
-    private String name;
+    @Column(name="name")
+    private String label;
 
     public Store() {
     }
 
-    public Store(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Store{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+    public Store(String value, String label) {
+        this.value = value;
+        this.label = label;
     }
 }
