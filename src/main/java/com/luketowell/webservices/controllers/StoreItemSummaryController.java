98a44b0e1f6f8e4f19431a7c966c732a432f6378
Luke Towell <luke.towell@hotmail.com>
2020-09-16 16:08:24 +0100

    updated the storeItemSummary COntroller
    - Updated the storeItemSummary controller to include the endpoint to retrieve categories using expiry date

--- src/main/java/com/luketowell/webservices/controllers/StoreItemSummaryController.java
+++ src/main/java/com/luketowell/webservices/controllers/StoreItemSummaryController.java
@@ -4,6 +4,7 @@ import com.luketowell.webservices.models.StoreItemSummary;
 import com.luketowell.webservices.repositories.StoreItemSummaryRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -27,5 +28,13 @@ public class StoreItemSummaryController {
     public List<StoreItemSummary> findAllItemsForStoreByCategory
             (@PathVariable String storeId, @PathVariable String itemUpc) {
         return storeItemSummaryRepository.getStoreItemsByItemUpcAndStoreId( itemUpc,storeId);
+    }
+
+    @GetMapping("/{storeId}/{categoryId}/{expiryDate}")
+    public List<StoreItemSummary> findAllItemsForStoreByCategoryForToday
+            (@PathVariable String storeId, @PathVariable Long categoryId, @PathVariable String expiryDate) {
+
+        System.out.println(expiryDate);
+        return storeItemSummaryRepository.getStoreItemsByCategoryAndStoreIdAndExpiryDate( categoryId, storeId, expiryDate);
     }
 }
